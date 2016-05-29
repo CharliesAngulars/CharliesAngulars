12a561b982f1370426bc97805da91fe21f149826
metamonkey8 <chen.liu.michael@gmail.com>
2016-05-29 14:45:30 -0700

    removed get request from server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express  = require('express');
 var mongoose = require('mongoose'); 
 var morgan = require('morgan');             
 var bodyParser = require('body-parser');    
-var methodOverride = require('method-override'); 
-var https = require('https');
+var methodOverride = require('method-override');
 
 var app = express();
 //mongodb to be connected once deployed with URI
@@ -40,23 +39,3 @@ var port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
-
-
-		
-var options = {
-  host: 'api.transitandtrails.org',
-  port: 443,
-  path: '/api/v1/campgrounds?key=1c78a948e0a02614d9caed392ee1388fc15e5eadc005ca69f7c451e80c02e1a0'
-};
-
-https.get(options, function(res) {
-  console.log("Got response: " + res.statusCode);
-
-  res.on("data", function(chunk) {
-    console.log("BODY: " + chunk);
-  });
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
-});
-	
-
